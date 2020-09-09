02f25fbb9b3f0ea7c4d242f1a89899f56c7cefaa
LucasCaravajal <caravajallucas@gmail.com>
2020-09-09 12:11:13 -0300

    mejoraPersonajes

--- js/adivinanza.js
+++ js/adivinanza.js
@@ -3,8 +3,8 @@ function jugar_ahora() {
   var min = 1;
   var cant_intentos = 3;
   var acerto = false;
-  var numero_aleatorio = Math.round(Math.random() * (max - min) + min);
-  var numero;
+  var numeroCorrecto = Math.round(Math.random() * (max - min) + min);
+  var numeroIngresado;
 
   var nombre = document.getElementById("nombre").value;
 
@@ -13,13 +13,13 @@ function jugar_ahora() {
   } else {
     while (cant_intentos > 0 && acerto != true) {
       cant_intentos--;
-      numero = parseInt(prompt("Ingrese el número"));
-      if (numero == numero_aleatorio) {
+      numeroIngresado = parseInt(prompt("Ingrese el número"));
+      if (numeroIngresado == numeroCorrecto) {
         alert("EUREKA!!");
         acerto = true;
-      } else if (numero < numero_aleatorio) {
+      } else if (numeroIngresado < numeroCorrecto) {
         alert("PISTA: es mayor");
-      } else if (numero > numero_aleatorio) {
+      } else if (numeroIngresado > numeroCorrecto) {
         alert("PISTA: es menor");
       } else {
         alert("Le quedan " + cant_intentos + " intentos");
@@ -27,7 +27,7 @@ function jugar_ahora() {
     }
 
     document.getElementById("rdo").innerHTML =
-      "El número era: <strong>" + numero_aleatorio + "</strong>";
+      "El número era: <strong>" + numeroCorrecto + "</strong>";
   }
 }
 
@@ -36,26 +36,4 @@ function play() {
   var correcta2 = "Lobo";
   var correcta3 = "Oso";
   var cant_intentos = 3;
-}
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
