288e113693e29ab396c39bc716aad327ddbdaf0c
LucasCaravajal <caravajallucas@gmail.com>
2020-09-10 22:06:06 -0300

    uppercase

--- js/adivinanza.js
+++ js/adivinanza.js
@@ -12,19 +12,23 @@ function play() {
       pregunta = prompt("¿Que animál da como botín -Una GARRA-?");
       intentos--;
       if (pregunta.toLowerCase() == respCorrecta) {
-        alert("¡Excelente! " + nombre + " ¡Haz menorizado la tabla muy bien!");
+        alert(
+          "¡Excelente! " +
+            nombre.toUpperCase() +
+            " ¡Haz menorizado la tabla muy bien!"
+        );
         acerto = true;
       } else if (intentos == 2) {
         alert(
           "¡Vamos! " +
-            nombre +
+            nombre.toUpperCase() +
             " ¡No te desanimes! Aquí va una pista: Es el animal mas grande que aparece en la tabla!"
         );
         alert("Le quedan " + intentos + " intentos!");
       } else if (intentos == 1) {
         alert("Aquí va la ultima pista: No siempre camina en cuatro patas!");
       } else {
-        alert("Lo sentimos " + nombre + " perdiste.");
+        alert("Lo sentimos " + nombre.toUpperCase() + " perdiste.");
       }
     }
     document.getElementById("rdo").innerHTML =
